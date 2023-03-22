f55dc35a9b861878742d9d9564f7acc67a8e1b10
csw531 <cwolcott@kershnertrading.com>
2023-03-22 09:17:19 -0400

    updated pomodoro

--- Modern Pomodoro.py
+++ Modern Pomodoro.py
@@ -1,4 +1,5 @@
 import customtkinter as ctk
+from tkinter import simpledialog
 
 class App:
     def __init__(self, master: ctk.CTk):
@@ -38,39 +39,58 @@ class App:
         self.reset_button.grid(row=0,column=2, padx=5,pady=5)
 
         # Initialize variables
-        self.focus_time = 25 * 60  # 25 minutes in seconds
-        self.break_time = 5 * 60   # 5 minutes in seconds
+        self.focus_time = 1 * 10  # 25 minutes in seconds
+        self.break_time = 1 * 10   # 5 minutes in seconds
         self.focus_count = 0
-        self.tags = []
         self.timer_running = False
         self.timer_paused = False
 
+        # Tagging
+        self.tag = None
+        self.tag_counter = {}
+
+        self.tag_frame = ctk.CTkFrame(self.master)
+        self.tag_frame.pack(pady=5)
+        self.tag_frame.pack_propagate(0)
+        self.tag_frame.configure(width=300)
+
+        self.tag_label = ctk.CTkLabel(self.tag_frame, text="", font=("Helvetica", 12), justify='left')
+        self.tag_label.pack()
 
     def do_countdown(self, remaining_time):
-            if self.timer_running == True:
-                self.remaining_time = remaining_time
-                mins, secs = divmod(remaining_time, 60)
-                self.timer_label.configure(text=f"{mins:02d}:{secs:02d}")
-                if remaining_time <= 0:
-                    self.timer_running = False
-                    self.focus_count += 1
-                    self.tags.append(self.get_tag())
-                    self.master.bell()  # Sound alarm
-                    if self.focus_count % 2 == 0:
-                        remaining_time = self.break_time
-                    else:
-                        remaining_time = self.focus_time
-                    self.do_countdown(remaining_time)
-                elif not self.timer_paused:
-                    self.master.after(1000, self.do_countdown, remaining_time - 1)
+        if self.timer_running == True:
+            self.remaining_time = remaining_time
+            mins, secs = divmod(remaining_time, 60)
+            self.timer_label.configure(text=f"{mins:02d}:{secs:02d}")
+            if remaining_time <= 0:
+                self.timer_running = False
+                self.timer_paused = False  # Added this line to ensure the timer is not in a paused state
+                self.stop_button.configure(state=ctk.DISABLED)  # Disable the Stop button
+                self.reset_button.configure(state=ctk.NORMAL)  # Enable the Reset button
+                self.focus_count += 1
+                self.master.bell()  # Sound alarm
+                if self.focus_count % 2 == 0:
+                    remaining_time = self.break_time
+                else:
+                    remaining_time = self.focus_time
+                self.do_countdown(remaining_time)
+                self.update_tag_counter(completed=True)  # Moved this line after resetting the remaining_time
+            elif not self.timer_paused:
+                self.after_event_id = self.master.after(1000, self.do_countdown, remaining_time - 1)  # Store the event ID 
 
+    
     def start_timer(self):
         if not self.timer_running:
-            self.timer_running = True
-            self.start_button.configure(state=ctk.DISABLED)
-            self.stop_button.configure(state=ctk.NORMAL)
-            self.reset_button.configure(state=ctk.NORMAL)
-            self.do_countdown(self.focus_time)          
+            self.tag = simpledialog.askstring("Task", "What are you working on?")
+            if self.tag is not None:
+                self.timer_running = True
+                self.start_button.configure(state=ctk.DISABLED)
+                self.stop_button.configure(state=ctk.NORMAL)
+                self.reset_button.configure(state=ctk.NORMAL)
+                self.do_countdown(self.focus_time) 
+                self.update_tag_counter(init=True)   
+        if not self.timer_paused:
+                self.after_event_id = self.master.after(1000, self.do_countdown, remaining_time - 1)  # Store the event ID
         elif self.timer_paused:
             self.timer_paused = False
             self.timer_running = True
@@ -79,9 +99,11 @@ class App:
             self.reset_button.configure(state=ctk.NORMAL)            
             self.do_countdown(self.remaining_time)
 
+
     def stop_timer(self):
         if not self.timer_paused:
-            self.timer_paused = True            
+            self.timer_paused = True 
+            self.master.after_cancel(self.after_event_id)  # Cancel the scheduled event                       
             self.start_button.configure(state=ctk.NORMAL)
             self.stop_button.configure(state=ctk.DISABLED)
             self.reset_button.configure(state=ctk.NORMAL)
@@ -89,17 +111,30 @@ class App:
             self.timer_running = False
     
     def reset_timer(self):
+        if self.timer_running and not self.timer_paused:
+            self.update_tag_counter()        
         self.timer_running = False
         self.timer_paused = False
         self.start_button.configure(state=ctk.NORMAL)
         self.stop_button.configure(state=ctk.DISABLED)
         self.reset_button.configure(state=ctk.DISABLED) 
         self.remaining_time = (25 * 60)
-        self.timer_label.configure(text="25:00")      
+        self.timer_label.configure(text="25:00") 
+        self.update_tag_counter(completed=False)             
             
-    def get_tag(self):
-        tag = ctk.simpledialog.askstring("Tag", "Enter a tag for this focus cycle:")
-        return tag if tag is not None else ""            
+    def update_tag_counter(self, init=False, completed=False):
+        if self.tag not in self.tag_counter:
+            self.tag_counter[self.tag] = {"complete": 0, "attempts": 1}
+        else:
+            if init:
+                self.tag_counter[self.tag]["attempts"] += 1
+            if completed:
+                self.tag_counter[self.tag]["complete"] += 1
+
+        tag_text = "Tag\tComplete\tAttempts\n"
+        for tag, stats in self.tag_counter.items():
+            tag_text += f"{tag}\t{stats['complete']}\t{stats['attempts']}\n"
+        self.tag_label.configure(text=tag_text.strip())           
 
 if __name__ == '__main__':
     app = ctk.CTk()
